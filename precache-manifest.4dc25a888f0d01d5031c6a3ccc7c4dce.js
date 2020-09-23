50d12245efe938a64b733baf074c7708090c3ed2
Satwik Goyal <65339762+satwikgoyal@users.noreply.github.com>
2020-09-23 23:58:46 +0530

    Updates

---
+++ precache-manifest.4dc25a888f0d01d5031c6a3ccc7c4dce.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "392e391b7955cb95e382240bfd0e750f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6f77cdd169636433da3c",
+    "url": "/portfolio/static/css/main.fd628d16.chunk.css"
+  },
+  {
+    "revision": "99a55c2e6f93f6719865",
+    "url": "/portfolio/static/js/2.eb34cbbf.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/portfolio/static/js/2.eb34cbbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f77cdd169636433da3c",
+    "url": "/portfolio/static/js/main.89d4360b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "1a502c3f19fbb24ac6089efcc3b8f77c",
+    "url": "/portfolio/static/media/avatar.1a502c3f.png"
+  },
+  {
+    "revision": "70a1a6a2637eb4244fb78323b337dad2",
+    "url": "/portfolio/static/media/b.70a1a6a2.jpg"
+  },
+  {
+    "revision": "e7243d9fead14f3025b95c1cde3055a0",
+    "url": "/portfolio/static/media/proj1.e7243d9f.jpg"
+  },
+  {
+    "revision": "cceb54b7f3cd6fe914feaf46048a3ee3",
+    "url": "/portfolio/static/media/proj2.cceb54b7.jpg"
+  },
+  {
+    "revision": "bfb776a2ce489549463c5af969e48a02",
+    "url": "/portfolio/static/media/proj3.bfb776a2.jpg"
+  },
+  {
+    "revision": "45f76d9a7d1c6f287929bba18972c6b3",
+    "url": "/portfolio/static/media/proj4.45f76d9a.jpg"
+  },
+  {
+    "revision": "763d44a9f0606b83cb2d313d368962dc",
+    "url": "/portfolio/static/media/proj5.763d44a9.jpg"
+  },
+  {
+    "revision": "805cc2f20699a73eb8984d0e1e2ab1e9",
+    "url": "/portfolio/static/media/proj6.805cc2f2.jpg"
+  }
+]);
